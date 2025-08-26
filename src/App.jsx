c876b6721722a680259cd12f6bf5276dde4f1680
TimothyCruzCk12 <timothy.cruz.intern@ck12.org>
2025-08-26 09:50:34 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PartsOfWhole from './components/PartsOfWhole'
+
+
+function App() {
+  return (
+      <div className="App">
+        <PartsOfWhole />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/PartsOfWhole.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef } from 'react';
+import confetti from 'canvas-confetti';
+import { Container } from './ui/reused-ui/Container.jsx'
+
+const PartsOfWhole = () => {
+
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default PartsOfWhole;
